3b0cc4c78e61e766892d33df744ebded8fbde123
Melissa O'Neill <oneill@acm.org>
2023-09-10 21:18:17 -0700

    Initial commit

---
+++ build.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# Run with: python3 build.py
+
+from os import system
+
+# run a command on the system
+def run(cmd):
+    print("Running:",cmd)
+    if (system(cmd) != 0):
+        raise RuntimeError("Command failed")
+
+run("clang++ -Wall -Wextra -pedantic -c -std=c++17 cow.cpp")
+run("clang++ -Wall -Wextra -pedantic -c -std=c++17 main.cpp")
+run("clang++ -std=c++17 -o main main.o cow.o")
+
+print("All done.")
